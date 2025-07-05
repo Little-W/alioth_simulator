7471f40a7c666cd6d4a2ff4a7021177f84b45382
Yusen <1405481963@qq.com>
2025-07-05 12:58:26 +0800

    [WIP] bpu: use BHT for jalr inst

--- deps/hardware-level/src/alioth/rtl/core/config.svh
+++ deps/hardware-level/src/alioth/rtl/core/config.svh
@@ -33,7 +33,9 @@
 `define DOUBLE_REG_WIDTH 64
 `define REG_NUM 32
 
-`define BPU_COUNT_WIDTH 4  // BPU计数器宽度
+// BTB配置
+`define BTB_INDEX_WIDTH 5  // BTB索引位宽
+`define BTB_SIZE (1 << `BTB_INDEX_WIDTH)  // BTB表项数量
 
 // 外设桥地址空间定义 (0x8010_0000~0x8020_0FFF)
 `define PERIP_BRIDGE_BASE_ADDR 32'h8010_0000
