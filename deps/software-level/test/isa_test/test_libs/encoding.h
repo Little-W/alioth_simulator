467441efcacad65d830f727f1fc5391e5f357629
Yusen <1405481963@qq.com>
2025-08-03 00:19:51 +0800

    feat: Update ISA tests and add float ISA tests
    - Updated the ISA tests to the latest version.
    - Added new test cases for single-precision and double-precision floating-point units (FPUs).

--- deps/software-level/test/isa_test/test_libs/encoding.h
+++ deps/software-level/test/isa_test/test_libs/encoding.h
@@ -341,34 +341,24 @@
 
 #ifdef __GNUC__
 
-#ifndef read_csr
 #define read_csr(reg) ({ unsigned long __tmp; \
   asm volatile ("csrr %0, " #reg : "=r"(__tmp)); \
   __tmp; })
-#endif
 
-#ifndef write_csr
 #define write_csr(reg, val) ({ \
   asm volatile ("csrw " #reg ", %0" :: "rK"(val)); })
-#endif
 
-#ifndef swap_csr
 #define swap_csr(reg, val) ({ unsigned long __tmp; \
   asm volatile ("csrrw %0, " #reg ", %1" : "=r"(__tmp) : "rK"(val)); \
   __tmp; })
-#endif
 
-#ifndef set_csr
 #define set_csr(reg, bit) ({ unsigned long __tmp; \
   asm volatile ("csrrs %0, " #reg ", %1" : "=r"(__tmp) : "rK"(bit)); \
   __tmp; })
-#endif
 
-#ifndef clear_csr
 #define clear_csr(reg, bit) ({ unsigned long __tmp; \
   asm volatile ("csrrc %0, " #reg ", %1" : "=r"(__tmp) : "rK"(bit)); \
   __tmp; })
-#endif
 
 #define rdtime() read_csr(time)
 #define rdcycle() read_csr(cycle)
