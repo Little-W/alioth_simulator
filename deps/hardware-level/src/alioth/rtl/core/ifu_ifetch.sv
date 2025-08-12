394f6c41f55aa0dd5c32252c1218dad5e386fb5d
Rumuh <18567772861@126.com>
2025-08-13 02:46:26 +0800

    fix misaligned

--- deps/hardware-level/src/alioth/rtl/core/ifu_ifetch.sv
+++ deps/hardware-level/src/alioth/rtl/core/ifu_ifetch.sv
@@ -39,7 +39,7 @@ module ifu_ifetch (
     // 新增输出：非对齐取指信号
     output wire pc_misaligned_o
 );
-
+    wire pc_misaligned;
     // 下一个PC值
     wire [`INST_ADDR_WIDTH-1:0] pc_nxt;
 
@@ -49,11 +49,22 @@ module ifu_ifetch (
     // 根据控制信号计算下一个PC值
     assign pc_nxt = (jump_flag_i == `JumpEnable) ? jump_addr_i :  // 跳转
         (stall_pc_actual) ? pc_o :  // 暂停（包括AXI未就绪的情况）
-        pc_o + 4'h8;  // 地址加8
+        (pc_misaligned) ? pc_o + 4'h4 : pc_o + 4'h8;  // 地址加4或加8
 
     // 64位访存中的PC对齐判断：检查PC[2]，因为64位访存时PC[2:0]中只有PC[2]有意义
     // PC[2]=0: 指令在低32位, PC[2]=1: 指令在高32位
-    assign pc_misaligned_o = pc_o[2];
+    assign pc_misaligned = pc_o[2];
+
+    // 延长一个周期的脉冲：如果当前周期或上一周期misaligned，则输出保持为1
+    reg pc_misaligned_last;  // 上一周期pc_misaligned
+    always @(posedge clk or negedge rst_n) begin
+        if (!rst_n) begin
+            pc_misaligned_last <= 1'b0;
+        end else begin
+            pc_misaligned_last <= pc_misaligned; // 采样当前周期
+        end
+    end
+    assign pc_misaligned_o = pc_misaligned | pc_misaligned_last;  // 输出延长一个周期
 
     // 定制PC寄存器，复位到正确地址
     reg [`INST_ADDR_WIDTH-1:0] pc_reg;
